f6ffc405261d59c2620a46bff20150250fba7d61
Andrew Chen <yongjhih@gmail.com>
2015-09-27 01:24:32 +0800

    tmux default shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,3 @@
 set fish_function_path $fish_function_path "/home/andrew/.local/lib/python3.4/site-packages/powerline/bindings/fish"
 powerline-setup
+# export PROMPT_COMMAND='echo -ne "\033k`basename "${PWD}"`\033\\"'
